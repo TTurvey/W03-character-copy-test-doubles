504a6859bea5d6925d936bda851b33fcaa895343
Hassan <hassansaleem194@gmail.com>
2022-09-19 11:32:51 +0100

    SetChar and destination and source tests added

--- src/test/java/CopierShould.java
+++ src/test/java/CopierShould.java
@@ -14,6 +14,24 @@ public class CopierShould {
         verify(source, atLeastOnce()).GetChar();
     }
 
+    @Test
+    void setDestination_SetChar_is_called_when_Copy_is_called() {
+        Copier copier = new Copier(source, destination);
+        copier.Copy();
+        verify(destination, atLeastOnce()).SetChar(anyChar());
+    }
+
+    @Test
+    void source_and_destination_should_have_the_same_character() {
+        char OUR_CHAR = 'n';
+
+        when(source.GetChar()).thenReturn(OUR_CHAR);
+        Copier copier = new Copier(source, destination);
+        copier.Copy();
+
+        verify(destination, atLeastOnce()).SetChar(OUR_CHAR);
+    }
+
 }
 
 
